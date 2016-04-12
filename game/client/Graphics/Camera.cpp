27a36994d71aad3c7ce72bd2430a0595b2e40d6f
ItsTheVu <cindyvudba12@gmail.com>
2016-04-12 16:44:56 -0700

    Simple camera class added. More functionality will be added when needed.

---
+++ game/client/Graphics/Camera.cpp
@@ -0,0 +1,32 @@
+#include "Camera.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+
+Camera::Camera() {
+
+	// Default position, target and up
+	position = glm::vec3(0.0f, 0.0f, 0.0f);
+	front = glm::vec3(0.0f, 0.0f, -20.0f);
+	direction = glm::normalize(front);
+	worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+}
+
+Camera::Camera(glm::vec3 pos, glm::vec3 tar, glm::vec3 world_up) {
+
+	position = pos;
+	front = tar;
+	direction = glm::normalize(front);
+	worldUp = world_up;
+
+}
+
+Camera::~Camera() {
+
+}
+
+glm::mat4 Camera::getView() {
+
+	return glm::lookAt(position, position + front, worldUp);
+
+}
