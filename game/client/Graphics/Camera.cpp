e281bbcb9473bbeb53b814070e07ef2c84fcabe4
Cindy K Vu <ckv001@UCSD.EDU>
2016-05-02 21:56:42 -0700

    Redoing camera

--- game/client/Graphics/Camera.cpp
+++ game/client/Graphics/Camera.cpp
@@ -1,33 +1,5 @@
 #include "Camera.h"
 
-#include <glm/gtc/matrix_transform.hpp>
+Camera::Camera() {}
 
-
-Camera::Camera() {
-	position = glm::vec3(0.f, 30.f, 70.f);
-	target = glm::vec3(0.0f, 20.0f, 0.f);
-	direction = glm::normalize(position - target);
-
-	up = glm::vec3(0.0f, 1.0f, 0.0f);
-	right = glm::normalize(glm::cross(up, direction));
-
-	cameraUp = glm::cross(direction, right);
-}
-
-Camera::~Camera() {
-
-}
-
-glm::mat4 Camera::getView() {
-	glm::mat4 view;
-	view = glm::lookAt(position, target, up);
-	return view;
-}
-
-glm::vec3 Camera::getPosition(){
-	return this->position;
-}
-
-glm::vec3 Camera::getDirection(){
-	return this->direction;
-}
+Camera::~Camera() {}
---
+++ game/client/Graphics/Camera_Original.cpp
@@ -0,0 +1,33 @@
+#include "Camera_Original.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+
+Camera_Original::Camera_Original() {
+	position = glm::vec3(0.f, 30.f, 70.f);
+	target = glm::vec3(0.0f, 20.0f, 0.f);
+	direction = glm::normalize(position - target);
+
+	up = glm::vec3(0.0f, 1.0f, 0.0f);
+	right = glm::normalize(glm::cross(up, direction));
+
+	Camera_OriginalUp = glm::cross(direction, right);
+}
+
+Camera_Original::~Camera_Original() {
+
+}
+
+glm::mat4 Camera_Original::getView() {
+	glm::mat4 view;
+	view = glm::lookAt(position, target, up);
+	return view;
+}
+
+glm::vec3 Camera_Original::getPosition(){
+	return this->position;
+}
+
+glm::vec3 Camera_Original::getDirection(){
+	return this->direction;
+}
