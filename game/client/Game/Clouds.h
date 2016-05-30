600afb99d576949efb33d6d5b23161fd8cc8d816
timothyk7 <timothyk7@gmail.com>
2016-05-30 02:44:32 -0700

    WIP, making clouds look more realistic

--- game/client/Game/Clouds.h
+++ game/client/Game/Clouds.h
@@ -61,7 +61,7 @@ private:
 	//cloud sky creation
 	const int NUMBER_OF_SPHERES = 1000;
 	const int SPHERES_PER_CLOUD = 1;
-	const float FLOATING_SPEED_PER_SECOND = 2.0f;
+	const float FLOATING_SPEED_PER_SECOND = 5.0f;
 	const float SKYBOX_LENGTH = 500; //pass in?
 	const glm::vec3 INITIAL_EXPANSION_DIRECTION = glm::vec3(1.0f, 0.0f, 1.0f);
 	const float MIN_CLOUD_CREATION_DISTANCE_SECONDS = 0.15f;
@@ -73,7 +73,7 @@ private:
 	const float MORPHING_SPEED_PER_SECOND = 0.1f;
 
 
-	glm::vec3 expansionDirection = INITIAL_EXPANSION_DIRECTION;
+	glm::vec3 expansionDirection = glm::vec3(1.0f, 0.0f, 0.0f);
 
 	void updateScene(float passedSeconds, float totalPassedTime);
 	void addCloud(int numberOfSpheres);
--- game/client/Game/Model.cpp
+++ game/client/Game/Model.cpp
@@ -227,16 +227,35 @@ GLint Model::TextureFromFile(const char* path, string directory)
 	string filename = string(path);
 	filename = directory + '/' + filename;
 	GLuint textureID;
-	glGenTextures(1, &textureID);
-	int width, height;
-	unsigned char* image = SOIL_load_image(filename.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
-	if (image == NULL || image[0] == '\0') {
+	//glGenTextures(1, &textureID);
+	//int width, height;
+	//unsigned char* image = SOIL_load_image(filename.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+	//if (image == NULL || image[0] == '\0') {
+	//	cout << "uh oh " << filename << endl;
+	//	return -1;
+	//}
+
+	textureID = SOIL_load_OGL_texture(filename.c_str(),
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		/*SOIL_FLAG_POWER_OF_TWO*/
+		SOIL_FLAG_MIPMAPS
+		/*| SOIL_FLAG_MULTIPLY_ALPHA*/
+		/*| SOIL_FLAG_COMPRESS_TO_DXT*/
+		/*| SOIL_FLAG_DDS_LOAD_DIRECT*/
+	);
+
+	if (textureID == 0) {
+		cout << "uh oh " << filename.c_str() << endl;
 		return -1;
 	}
+
 	// Assign texture to ID
 	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//glGenerateMipmap(GL_TEXTURE_2D);
+
+
 
 	// Parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
