1ae803180b0be064d6abebb76b8548a27f526908
ckv001 <ckv001@ACS-CSEB220-17>
2016-06-02 22:47:43 -0700

    Fixed hat issue

--- game/client/Game/Player.cpp
+++ game/client/Game/Player.cpp
@@ -102,7 +102,7 @@ void Player::changeState(protos::Message_GameObject_AnimationState newState){
 
 	float scaleFactorPlayer = 0.075f;
 	float scaleFactorHat = 2.2f;
-	float transZ = currState == protos::Message_GameObject_AnimationState_STANDING ? 0.0f : 0.5f;
+	float transZ = currState == protos::Message_GameObject_AnimationState_STANDING || currState == protos::Message_GameObject_AnimationState_FLYING ? 0.0f : 0.5f;
 
 	//z = 0.5f to put hat in right spot 
 	glm::mat4 translate = glm::translate(glm::mat4(1.f), glm::vec3(0.f, (playerModels[protos::Message_GameObject_AnimationState_STANDING]->getPlayerModel()->getHeight()*scaleFactorPlayer) - 0.6f, transZ));
--- game/client/Window.cpp
+++ game/client/Window.cpp
@@ -59,8 +59,8 @@ GLFWwindow* Window::create_window(int width, int height) {
 	glfwWindowHint(GLFW_SAMPLES, 4);
 
 	// Create the GLFW window
-	//GLFWwindow* window = glfwCreateWindow(width, height, window_title, glfwGetPrimaryMonitor(), NULL);
-     GLFWwindow* window = glfwCreateWindow(width, height, window_title, NULL, NULL);
+	 GLFWwindow* window = glfwCreateWindow(width, height, window_title, glfwGetPrimaryMonitor(), NULL);
+     //GLFWwindow* window = glfwCreateWindow(width, height, window_title, NULL, NULL);
 
 	// Check if the window could not be created
 	if (!window) {
